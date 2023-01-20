ba8f4976a9561f3248f0363bc50caf1ea1dd41d2
KayceeNguyen <nnguyen106@my.bcit.ca>
2023-01-20 01:48:00 -0800

    trying to fix container disappear error

--- .next/static/webpack/webpack.a5c810d97ed35d6e.hot-update.js
+++ .next/static/webpack/webpack.a5c810d97ed35d6e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5422f464585e1661"; }
+/******/ 	__webpack_require__.h = function() { return "12a049a290b043db"; }
 /******/ }();
 /******/ 
 /******/ }
--- .next/static/webpack/webpack.ce2827be229d1025.hot-update.js
+++ .next/static/webpack/webpack.ce2827be229d1025.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ce26dd78d4aed104"; }
+/******/ 	__webpack_require__.h = function() { return "d14109f36e3f1950"; }
 /******/ }();
 /******/ 
 /******/ }
