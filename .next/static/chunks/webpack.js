d60167b8cffe1d0f91fb9fcbddce270d3e22d80a
KayceeNguyen <nnguyen106@my.bcit.ca>
2023-01-20 01:55:05 -0800

    fixed map disappear by comment out setMaxListeners

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e943afd902f46079"; }
+/******/ 		__webpack_require__.h = function() { return "6b74cc1970dcb89f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.83443104beae4b54.hot-update.js
+++ .next/static/webpack/webpack.83443104beae4b54.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bc93141167ddeeb9"; }
+/******/ 	__webpack_require__.h = function() { return "eeb50f52927d4e57"; }
 /******/ }();
 /******/ 
 /******/ }
